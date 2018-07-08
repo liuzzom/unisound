5cf8c27a6487e8cb6a05a20997d32e24773f4771
liuzzom <antoninomauro.liuzzo@community.unipa.it>
2018-07-08 18:04:46 +0200

    aggiunta dinamicità ai bottoni e alla finestra home

--- js/client/song_listening/menu.js
+++ js/client/song_listening/menu.js
@@ -1,5 +1,64 @@
 function menuToggle(){
+    // flag per consentire la visualizzazione dei pannelli
+    var playlist_shown = false, friends_shown = false;
+
+    // click al pulsante playlist
+    $('.playlist_button').on('click', function(){
+        // il pannello degli amici è nascosto, visualizza quello delle playlist
+        if(!friends_shown){
+            $('.leftpane').slideToggle(400);
+            $('.middlepane').slideToggle(400);
+            document.getElementById('leftpane').style.height = "100%";
+            document.getElementById('leftpane').style.width = "100%";
+            if(!playlist_shown){
+                playlist_shown = true;
+            }else{
+                playlist_shown = false;
+            }
+        }
+    });
+
+    // click al pulsante amici
+    $('.friends_button').on('click', function(){
+        // il pannello delle playlist è nascosto, visualizza quello degli amici
+        if(!playlist_shown){
+            $('.rightpane').slideToggle(400);
+            $('.middlepane').slideToggle(400);
+            document.getElementById('rightpane').style.height = "100%";
+            document.getElementById('rightpane').style.width = "100%";
+            if(!friends_shown){
+                friends_shown = true;
+            }else{
+                friends_shown = false;
+            }
+        }
+    });
+
+    // callback per la media query che mostra i pannelli (ritorno a grandi dimensioni)
+    function show_panes(screen_show){
+        if(screen_show.matches){
+            document.getElementById('leftpane').style.width = "20%";
+            document.getElementById('rightpane').style.width = "20%";
+            $('.leftpane').show();
+            $('.rightpane').show();
+        }
+    }
+
+    var screen_show = window.matchMedia("(min-width: 800px)");
+    show_panes(screen_show); // Call listener function at run time
+    screen_show.addListener(show_panes); // Attach listener function on state changes
+
+    // callback per la media query che nasconde i pannelli (ritorno a piccole dimensioni)
+    function hide_panes(screen_hide){
+        if(screen_hide.matches){
+            $('.leftpane').hide();
+            $('.rightpane').hide();
+        }
+    }
     
+    var screen_hide = window.matchMedia("(max-width: 800px)");
+    hide_panes(screen_hide);
+    screen_hide.addListener(hide_panes);
 }
 
 $(document).ready(menuToggle);
