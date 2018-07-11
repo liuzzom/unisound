9d15f7d10625ae28a645ac69d00987b31c6c79ea
liuzzom <antoninomauro.liuzzo@community.unipa.it>
2018-07-11 19:26:01 +0200

    Modificato user.js e inseriti set e get per parametri disponibili in scrittura

--- js/client/models/user.js
+++ js/client/models/user.js
@@ -3,32 +3,76 @@ function User(id, first_name, last_name, email, password, online) {
         'id' : {
             value : id,
             writable : false
-        },
-
-        'first_name' : {
-            value : first_name,
-            writable : false
-        },
-
-        'last_name' : {
-            value : last_name,
-            writable : false
-        },
-
-        'email' : {
-            value : email,
-            writable : false
-        },
-
-        'password' : {
-            value : password,
-            writable : false
-        },
-
-        // valore booleano che verifica se l'utente è online o meno
-        'online' : {
-            value : online,
-            writable : false
         }
     });
+    this._first_name = first_name;
+    this._last_name = last_name;
+    this._email = email;
+    this._password = password;
+    this._online = online;
 }
+
+Object.defineProperty(User.prototype, 'first_name', {
+    set : function(first_name){
+        if(typeof first_name === 'string'){
+            this._first_name = first_name;
+        }else{
+            throw new Error("Inserire nome valdo");
+        }
+    },
+    get : function(){
+        return this._first_name;
+    }
+});
+
+Object.defineProperty(User.prototype, 'last_name', {
+    set : function(last_name){
+        if(typeof last_name === 'string'){
+            this._last_name = last_name;
+        }else{
+            throw new Error("Inserire nome valdo");
+        }
+    },
+    get : function(){
+        return this._last_name;
+    }
+});
+
+Object.defineProperty(User.prototype, 'email', {
+    set : function(email){
+        if(typeof email === 'string'){
+            this._email = email;
+        }else{
+            throw new Error("Inserire nome valdo");
+        }
+    },
+    get : function(){
+        return this._email;
+    }
+});
+
+Object.defineProperty(User.prototype, 'password', {
+    set : function(password){
+        if(typeof password === 'string'){
+            this._password = password;
+        }else{
+            throw new Error("Inserire nome valdo");
+        }
+    },
+    get : function(){
+        return this._password;
+    }
+});
+
+Object.defineProperty(User.prototype, 'online', {
+    set : function(online){
+        if(typeof online === 'boolean'){
+            this._online = online;
+        }else{
+            throw new Error("Inserire nome valdo");
+        }
+    },
+    get : function(){
+        return this._online;
+    }
+});
