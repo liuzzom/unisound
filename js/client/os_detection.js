c25f9670b12cbec486b4ea3ba7bcafcc1268fcec
Scaldabagno <federico.augello@community.unipa.it>
2018-07-12 15:29:15 +0200

    Fixed os_detection.js

--- js/client/os_detection.js
+++ js/client/os_detection.js
@@ -7,12 +7,19 @@ function osDetect(){
     // "Unknown OS" indicates failure to detect the OS
 
     var OSName="Unknown OS";
-    if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
-    if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
-    if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
-    if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
-    
-    if (OSName="UNIX"){
+    if (navigator.appVersion.indexOf("Win")!=-1){ 
+        OSName="Windows";
+    }
+
+    if (navigator.appVersion.indexOf("Mac")!=-1){ 
+        OSName="MacOS";
+    }
+    if (navigator.appVersion.indexOf("X11")!=-1){ 
+        OSName="UNIX";
+        $('header').attr('class', 'linux_visual');
+    }
+    if (navigator.appVersion.indexOf("Linux")!=-1){
+        OSName="Linux";
         $('header').attr('class', 'linux_visual');
     }
 }
